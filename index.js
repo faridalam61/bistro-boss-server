406adfb1ad16a782e4e1bfa80c258b1fa4912e9c
Farid Alam <faridalam@Farids-MacBook-Air.local>
2023-06-03 01:52:04 +0600

    Cart completed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
@@ -48,7 +48,22 @@ async function run() {
     }) 
 // Get carts
 app.get('/carts',async (req,res)=>{
-  
+  const email = req.query.email;
+  if(!email){
+    res.send([])
+  } 
+
+  const query = {email : email}
+  const resutl = await cartCollection.find(query).toArray()
+  res.send(resutl)
+})
+
+// Delete cart item
+app.delete('/carts/:id', async (req,res)=>{
+  const id = req.params.id;
+  const query = {_id : new ObjectId(id)}
+  const result = await cartCollection.deleteOne(query);
+  res.send(result)
 })
 
 
